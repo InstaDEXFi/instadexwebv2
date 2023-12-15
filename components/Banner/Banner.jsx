58e9ed375146c4e4692c2169f6ac5f5065583c0f
SNNICKETH <ddmarrrkov@outlook.com>
2023-12-15 23:56:08 +0100

    Update Banner.jsx

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -21,8 +21,6 @@ const Banner = () => {
           className="img-three"
         />
       </div>
-
-
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-10">
@@ -34,38 +32,6 @@ const Banner = () => {
               Makes DEX trading simple, accessible, and affordable on <span>Ethereum</span> L1 and L2’s.
               </h2>
             </div>
-            <div className="banner-progress-wrap">
-              <ul>
-                <li>Pre Sell</li>
-                <li>Soft Cap</li>
-                <li>Bonus</li>
-              </ul>
-              <div className="progress">
-                <div
-                  className="progress-bar"
-                  role="progressbar"
-                  style={{ width: "70" }}
-                  aria-valuenow="0"
-                  aria-valuemin="0"
-                  aria-valuemax="0"
-                />
-              </div>
-              <h4 className="title">
-                0% target raised <span>1 USDT - 0.00044 ETH = 45 $IDEX Tokens</span> <span>5M $IDEX Tokens for ICO</span>
-              </h4>
-            </div>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-xl-10">
-            <div className="banner-countdown-wrap text-center">
-              <h2 className="title">ICO Will Start In..</h2>
-
-              <CountDownOne />
-            </div>
-          </div>
-          </div>
-        </div>
         <div className="row justify-content-center">
           <div className="col-xl-10">
           </div>
