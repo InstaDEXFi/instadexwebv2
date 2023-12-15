c2ddff4c0a0b9d7e3b15af443836bfa319b67698
SNNICKETH <ddmarrrkov@outlook.com>
2023-12-15 22:36:09 +0100

    #update2

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -4,6 +4,24 @@ import CountDownOne from "../CountDown/CountDownOne";
 const Banner = () => {
   return (
     <section className="banner-area banner-bg">
+            <div className="banner-shape-wrap">
+        <img
+          src={"/img/banner/banner_shape01.png"}
+          alt=""
+          className="img-one"
+        />
+        <img
+          src={"/img/banner/banner_shape02.png"}
+          alt=""
+          className="img-two"
+        />
+        <img
+          src={"/img/banner/banner_shape03.png"}
+          alt=""
+          className="img-three"
+        />
+      </div>
+
 
       <div className="container">
         <div className="row justify-content-center">
@@ -16,13 +34,42 @@ const Banner = () => {
               Makes DEX trading simple, accessible, and affordable on <span>Ethereum</span> L1 and L2’s.
               </h2>
             </div>
+            <div className="banner-progress-wrap">
+              <ul>
+                <li>Pre Sell</li>
+                <li>Soft Cap</li>
+                <li>Bonus</li>
+              </ul>
+              <div className="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  style={{ width: "75%" }}
+                  aria-valuenow="75"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                />
+              </div>
+              <h4 className="title">
+                0% target raised <span>1 USDT - 0.00044 ETH = 45 $IDEX Tokens</span>
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-xl-10">
+            <div className="banner-countdown-wrap text-center">
+              <h2 className="title">ICO Will Start In..</h2>
+
+              <CountDownOne />
+            </div>
+          </div>
           </div>
         </div>
         <div className="row justify-content-center">
           <div className="col-xl-10">
           </div>
         </div>
-      </div>
     </section>
   );
 };
