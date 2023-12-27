58de9b05799a0e00e78f67143c483373dad45788
SNNICKETH <ddmarrrkov@outlook.com>
2023-12-27 19:23:46 +0100

    Update FooterOne.jsx

--- components/Footer/FooterOne.jsx
+++ components/Footer/FooterOne.jsx
@@ -126,7 +126,7 @@ const FooterOne = () => {
             <div className="row align-items-center">
               <div className="col-lg-6">
                 <div className="copyright-text">
-                  <p>Copyright &copy; 2023. All Rights Reserved InstaDEX.finance</p>
+                  <p>Copyright &copy; 2023. All Rights Reserved InstaDEX.Finance</p>
                 </div>
               </div>
               <div className="col-lg-6 d-none d-sm-block">
