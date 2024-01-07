eba2a4de4ddbfe0a784b45f36b21f6b7f89690c6
SNNICKETH <ddmarrrkov@outlook.com>
2024-01-07 16:28:07 +0100

    Update SalesTabContent.jsx

--- components/Sales/SalesTabContent.jsx
+++ components/Sales/SalesTabContent.jsx
@@ -13,8 +13,8 @@ const SalesTabContent = (props) => {
       <div className="chart-content-inner">
         <h2 className="title">{props.title}</h2>
         <p>{props.description}</p>
-        <Link href="" className="btn">
-          Buy Now
+        <Link href="https://www.pinksale.finance/launchpad/0x9863F54b8a85101B4E8eBDc5101c70C41206008f?chain=ETH" className="btn">
+          Buy Now On PinkSale
         </Link>
       </div>
     </div>
