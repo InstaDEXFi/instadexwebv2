a1e5474880c51d6c0dfb90114fab41006c5edf07
SNNICKETH <ddmarrrkov@outlook.com>
2024-01-07 16:34:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import WhoWeAre from "@/components/WhoWeAre/WhoWeAre";
 import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
 import LayoutOne from "@/layouts/LayoutOne";
 import Faq from "@/components/Faq/Faq";
-import CountDownOne from "@/componets/CountDown/CountDownOne;
+import Countdown from "@/componets/CountDown/CountDownOne;
 
 export default function Home() {
   return (
@@ -20,7 +20,7 @@ export default function Home() {
 
       <LayoutOne>
         <main className="fix">
-          <CountDownOne />
+          <Countdown />
           <Banner />
           <WhoWeAre />
 
